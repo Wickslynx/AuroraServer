b4159c1c0ca8e54a5ec29497c0cfee891494946e
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2024-12-06 11:43:08 +0100

    Create routes.c

---
+++ src/routes.c
@@ -0,0 +1,9 @@
+// routes.h
+#ifndef ROUTES_H
+#define ROUTES_H
+
+void load_web_files(const char *path_js, const char *path_css, const char *path_html);
+void route_handler(const char *method, const char *path);
+// Add more function declarations as needed
+
+#endif // ROUTES_H
